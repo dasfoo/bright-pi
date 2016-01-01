d8103827218904766c066c68ba8ac07f756290ef
Artem Sheremet <dot.doom@gmail.com>
2016-01-01 15:16:11 +0100

    Implement Power, Dim and Gain for LEDs

--- bpi.go
+++ bpi.go
@@ -1,11 +1,6 @@
 package bpi
 
-// Code examples taken from
-// https://www.pi-supply.com/bright-pi-v1-0-code-examples/
-
-import (
-	"github.com/dasfoo/i2c"
-)
+import "github.com/dasfoo/i2c"
 
 // BrightPI i2c wrapper. Built from code examples at:
 // https://www.pi-supply.com/bright-pi-v1-0-code-examples/
@@ -22,34 +17,48 @@ func NewBrightPI(i2c *i2c.Bus, bpiAddr byte) *BrightPI {
 	return &BrightPI{i2c: i2c, bpiAddr: bpiAddr}
 }
 
-/*
-* Then you need to turn the gain up to full using:
-*
-* sudo i2cset -y 1 0x70 0x09 0x0f
-*
-* Then you need to turn brightness of individual LEDs up. For white:
-*
-* sudo i2cset -y 1 0x70 0x02 0x32
-* sudo i2cset -y 1 0x70 0x04 0x32
-* sudo i2cset -y 1 0x70 0x05 0x32
-* sudo i2cset -y 1 0x70 0x07 0x32
-*
-* For IR:
-*
-* sudo i2cset -y 1 0x70 0x01 0x32
-* sudo i2cset -y 1 0x70 0x03 0x32
-* sudo i2cset -y 1 0x70 0x06 0x32
-* sudo i2cset -y 1 0x70 0x08 0x32
-* */
+// Led color and position
+const (
+	WhiteTopLeft     byte = 1 << 1
+	WhiteBottomLeft       = 1 << 3
+	WhiteBottomRight      = 1 << 4
+	WhiteTopRight         = 1 << 6
+	WhiteAll              = WhiteTopLeft + WhiteBottomLeft + WhiteBottomRight + WhiteTopRight
+	IRBottomLeft          = 1 << 0
+	IRTopLeft             = 1 << 2
+	IRTopRight            = 1 << 5
+	IRBottomRight         = 1 << 7
+	IRAll                 = IRTopLeft + IRBottomLeft + IRBottomRight + IRTopRight
+)
 
-// All lights configured at once
-func (p *BrightPI) All(white, ir bool) error {
-	var cmd byte
-	if white {
-		cmd += 0xa5
+// Max and default levels of Dim and Gain
+const (
+	MaxDim     = 0x3f
+	DefaultDim = 0x01
+
+	MaxGain     = 0x0f
+	DefaultGain = 0x08
+)
+
+// Power setting for the specified LEDs (others are turned off)
+func (p *BrightPI) Power(leds byte) error {
+	return p.i2c.WriteByteToReg(p.bpiAddr, 0x00, leds)
+}
+
+// Dim one or multiple LEDs (value range 0-MaxDim, default DefaultDim)
+func (p *BrightPI) Dim(leds, value byte) error {
+	var i byte
+	for i = 0; i < 8; i++ {
+		if leds&(1<<i) > 0 {
+			if err := p.i2c.WriteByteToReg(p.bpiAddr, i+1, value); err != nil {
+				return err
+			}
+		}
 	}
-	if ir {
-		cmd += 0x5a
-	}
-	return p.i2c.WriteByteToReg(p.bpiAddr, 0x00, cmd)
+	return nil
+}
+
+// Gain overall LEDs brightness (value range 0-MaxGain, default DefaultGain)
+func (p *BrightPI) Gain(value byte) error {
+	return p.i2c.WriteByteToReg(p.bpiAddr, 0x09, value)
 }
